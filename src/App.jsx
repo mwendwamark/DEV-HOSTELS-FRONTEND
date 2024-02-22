2958db94de5f0bfb57b6ab6b32f3175cb4f758dc
mark mwendwa <mwendwamark310@gmail.com>
2024-02-22 19:23:15 +0300

    create the login page and make it responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./Components/Navbar";
+import LoginPage from "./Pages/LoginPage";
+import SignupPage from "./Pages/SignupPage";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Navbar />
+        <Routes>
+          <Route path="/login" element={<LoginPage />}></Route>
+          <Route path="/signup" element={<SignupPage />}></Route>
+        </Routes>
       </BrowserRouter>
     </>
   );
